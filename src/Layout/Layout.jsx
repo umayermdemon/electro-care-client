0fbcadf46fa3d95c7856a04bbbd2a2d9534dbb91
EMON <mamudmdemon@gmail.com>
2024-05-12 10:10:07 +0600

    react router dom basic setup complete

---
+++ src/Layout/Layout.jsx
@@ -0,0 +1,11 @@
+
+
+const Layout = () => {
+  return (
+    <div>
+      <h1>This is layout</h1>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,10 @@
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <h1>This is error page</h1>
+    </div>
+  );
+};
+
+export default ErrorPage;
